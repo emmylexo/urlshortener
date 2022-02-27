db3343ba238f166e74465412cffd91cd7efe06cb
Abeng Emmanuel <emmanuelabeng27@gmail.com>
2022-02-27 14:59:05 +0100

    Updated test for watching

---
+++ src/app/modules/shortner/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { joiValidator } from "iyasunday";
+
+const route = Router();
+
+route.post(
+    "/fixture",
+    // controller.create
+);
+
+export default route;
